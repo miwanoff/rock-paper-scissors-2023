d9385449f76fcbdc7a9e8861794f767e26140ca1
miwanoff <marina.iva.noffa@gmail.com>
2023-05-21 13:07:20 +0300

    Add express static content style.css client.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const path = require('path');
 
 const server = http.createServer(app);
 
 app.get("/healthcheck", (req, res) => {
   res.send("<h2>RPS App running...</h2>");
 });
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/client/index.html");
+});
+
+app.use(express.static(path.join(__dirname, 'client')));
 
 server.listen(3000, () => {
   console.log("listening on *:3000");
