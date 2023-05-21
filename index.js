9ed292da2ccda241cc39356cb331038d95a3159e
miwanoff <marina.iva.noffa@gmail.com>
2023-05-21 13:17:00 +0300

    Fix server init before using

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const app = express();
 const http = require("http");
-const path = require('path');
+const path = require("path");
+const { Server } = require("socket.io");
 
+
+
+const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
 app.get("/healthcheck", (req, res) => {
   res.send("<h2>RPS App running...</h2>");
@@ -13,7 +17,11 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client/index.html");
 });
 
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, "client")));
+
+io.on("connection", (socket) => {
+  console.log("a user is connected");
+});
 
 server.listen(3000, () => {
   console.log("listening on *:3000");
