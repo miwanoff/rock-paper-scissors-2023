2cc534c6e4c77a39eead2c0641dc10a7b63accf0
miwanoff <marina.iva.noffa@gmail.com>
2023-05-27 12:30:51 +0300

    Add Copy Code Button

--- client/client.js
+++ client/client.js
@@ -3,7 +3,7 @@ const socket = io();
 let roomUniqueId = null;
 
 function сreateGame() {
-    console.log("сreateGame1");
+  console.log("сreateGame1");
   socket.emit("сreateGame");
 }
 
@@ -16,7 +16,22 @@ socket.on("newGame", (data) => {
   roomUniqueId = data.roomUniqueId;
   document.getElementById("initial").style.display = "none";
   document.getElementById("gamePlay").style.display = "block";
+  let copyButton = document.createElement("button");
+  copyButton.style.display = "block";
+  copyButton.innerText = "Copy Code";
+  copyButton.addEventListener("click", () => {
+    navigator.clipboard.writeText(roomUniqueId).then(
+      function () {
+        console.log("Async: Copying to clipboard was successful!");
+      },
+      function (err) {
+        console.error("Async: Could not copy text: ", err);
+      }
+    );
+  });
+
   document.getElementById(
     "waitingArea"
   ).innerHTML = `Waiting for opponent, please share code ${roomUniqueId} to join`;
+  document.getElementById("waitingArea").appendChild(copyButton);
 });
