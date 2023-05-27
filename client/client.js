684b99ed5c8952b425ef3435b57c29ad843b0aac
miwanoff <marina.iva.noffa@gmail.com>
2023-05-27 12:55:38 +0300

    Add sendChoice

--- client/client.js
+++ client/client.js
@@ -2,7 +2,10 @@ console.log("client.js loaded");
 const socket = io();
 let roomUniqueId = null;
 
+let player1 = false;
+
 function сreateGame() {
+  player1 = true;
   console.log("сreateGame1");
   socket.emit("сreateGame");
 }
@@ -10,6 +13,20 @@ function сreateGame() {
 function joinGame() {
   roomUniqueId = document.getElementById("roomUniqueId").value;
   socket.emit("joinGame", { roomUniqueId: roomUniqueId });
+}
+
+function sendChoice(rpsValue) {
+  const choiceEvent = player1 ? "p1Choice" : "p2Choice";
+  socket.emit(choiceEvent, {
+    rpsValue: rpsValue,
+    roomUniqueId: roomUniqueId,
+  });
+  let playerChoiceButton = document.createElement("button");
+  playerChoiceButton.style.display = "block";
+  playerChoiceButton.classList.add(rpsValue.toString().toLowerCase());
+  playerChoiceButton.innerText = rpsValue;
+  document.getElementById("player1Choice").innerHTML = "";
+  document.getElementById("player1Choice").appendChild(playerChoiceButton);
 }
 
 socket.on("newGame", (data) => {
@@ -36,8 +53,8 @@ socket.on("newGame", (data) => {
   document.getElementById("waitingArea").appendChild(copyButton);
 });
 
-socket.on('playersConnected', (data) => {
+socket.on("playersConnected", (data) => {
+  document.getElementById("initial").style.display = "none";
   document.getElementById("waitingArea").style.display = "none";
-  document.getElementById('gameArea').style.display = 'block';
-
+  document.getElementById("gameArea").style.display = "block";
 });
